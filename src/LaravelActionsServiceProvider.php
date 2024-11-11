1eccbae287c7786df59a91b299a5089de9adc45e
Yuriy Maslennikov <t-w-i@ya.ru>
2024-11-11 20:40:49 +0300

    first commit

--- src/LaravelActionsServiceProvider.php
+++ src/LaravelActionsServiceProvider.php
@@ -3,8 +3,16 @@
 namespace Maslennikov\LaravelActions;
 
 use Illuminate\Support\ServiceProvider;
+use Maslennikov\LaravelActions\Commands\ActionMakeCommand;
 
 class LaravelActionsServiceProvider extends ServiceProvider
 {
-
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ActionMakeCommand::class,
+            ]);
+        }
+    }
 }
