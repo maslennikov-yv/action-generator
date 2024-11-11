b9a10a0ed9b6128b7f0fdda83dd73dce0dcecfba
Yuriy Maslennikov <t-w-i@ya.ru>
2024-11-11 20:45:07 +0300

    first commit

--- src/Commands/ActionMakeCommand.php
+++ src/Commands/ActionMakeCommand.php
@@ -6,6 +6,20 @@ use Illuminate\Console\GeneratorCommand;
 
 class ActionMakeCommand extends GeneratorCommand
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
     protected function getStub()
     {
         return __DIR__ . '/stubs/test.stub';
