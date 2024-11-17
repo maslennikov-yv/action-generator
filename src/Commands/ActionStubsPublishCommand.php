433db5a33d21b4dcda47b4907d825abe6a886940
Yuriy Maslennikov <t-w-i@ya.ru>
2024-11-17 15:48:30 +0300

    action:stub:publish

--- src/Commands/ActionStubsPublishCommand.php
+++ src/Commands/ActionStubsPublishCommand.php
@@ -33,12 +33,35 @@ class ActionStubsPublishCommand extends Command
         }
 
         $stubs = [
+            // action
             realpath(__DIR__ . '/../stubs/action.stub') => 'action.stub',
             realpath(__DIR__ . '/../stubs/action.create.stub') => 'action.create.stub',
             realpath(__DIR__ . '/../stubs/action.destroy.stub') => 'action.destroy.stub',
             realpath(__DIR__ . '/../stubs/action.index.stub') => 'action.index.stub',
             realpath(__DIR__ . '/../stubs/action.show.stub') => 'action.show.stub',
             realpath(__DIR__ . '/../stubs/action.update.stub') => 'action.update.stub',
+            // action.controller
+            realpath(__DIR__ . '/../stubs/action.controller.dataset.destroy.stub') => 'action.controller.dataset.destroy.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.dataset.show.stub') => 'action.controller.dataset.show.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.dataset.store.stub') => 'action.controller.dataset.store.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.dataset.stub') => 'action.controller.dataset.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.dataset.update.stub') => 'action.controller.dataset.update.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.stub') => 'action.controller.stub',
+            realpath(__DIR__ . '/../stubs/action.controller.test.stub') => 'action.controller.test.stub',
+            // action.data
+            realpath(__DIR__ . '/../stubs/action.data.stub') => 'action.data.stub',
+            realpath(__DIR__ . '/../stubs/action.data.create.stub') => 'action.data.create.stub',
+            realpath(__DIR__ . '/../stubs/action.data.destroy.stub') => 'action.data.destroy.stub',
+            realpath(__DIR__ . '/../stubs/action.data.show.stub') => 'action.data.show.stub',
+            // action.dataset
+            realpath(__DIR__ . '/../stubs/action.dataset.stub') => 'action.dataset.stub',
+            realpath(__DIR__ . '/../stubs/action.dataset.create.stub') => 'action.dataset.create.stub',
+            realpath(__DIR__ . '/../stubs/action.dataset.destroy.stub') => 'action.dataset.destroy.stub',
+            realpath(__DIR__ . '/../stubs/action.dataset.update.stub') => 'action.dataset.update.stub',
+            // action.test
+            realpath(__DIR__ . '/../stubs/action.test.stub') => 'action.test.stub',
+            realpath(__DIR__ . '/../stubs/action.test.destroy.stub') => 'action.test.destroy.stub',
+            realpath(__DIR__ . '/../stubs/action.test.index.stub') => 'action.test.index.stub',
         ];
 
         foreach ($stubs as $from => $to) {
